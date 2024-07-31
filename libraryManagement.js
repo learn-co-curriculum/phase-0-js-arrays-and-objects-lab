28581fd40c15a17f75d4270ddba082b43e854540
Nick Christensen <nickachristensen@gmail.com>
2024-07-31 09:13:40 -0400

    Remove solution

--- libraryManagement.js
+++ libraryManagement.js
@@ -1,24 +1,8 @@
-let books = [];
+//Write code here
 
-function addBook(title, author, year, status) {
-  let book = { title: title, author: author, year: year, status: status };
-  books.push(book);
-}
 
-function checkoutBook(title) {
-  for (let book of books) {
-    if (book.title === title) {
-      if (book.status === 'available') {
-        book.status = 'checked out';
-      } else {
-        console.log(`The book "${title}" is already checked out.`);
-      }
-      return;
-    }
-  }
-  console.log(`The book "${title}" is not found in the library.`);
-}
 
+//Needed for the test. Don't modify.
 module.exports = {
   ...(typeof books !== 'undefined' && { books }),
   ...(typeof addBook !== 'undefined' && { addBook }),
